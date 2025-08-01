c097917d3c0559aeabefff2d7c83b4abd4432838
Brian Miano <brianmiano1@gmail.com>
2025-08-02 00:45:12 +0100

    the different models needed to be registered on admin.py file of bookshelf when I had done it on relationship app

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,16 +1,25 @@
+# relationship_app/admin.py
+
 from django.contrib import admin
-from .models import Book # Import your Book model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin 
+from .models import Author, Book, Library, Librarian, CustomUser # UPDATED: Import CustomUser
+from django.contrib.auth.models import Group # Import Group model to unregister it if needed
 
-# Register your models here.
+# Register your existing models
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
 
-# Option 1: Basic Registration (no customization yet)
-# admin.site.register(Book)
 
-# Option 2: Registration with Customization (as per the next step)
-# We'll use this for the next part of the task
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year') # Display these fields in the list view
-    list_filter = ('publication_year', 'author') # Add filters for these fields
-    search_fields = ('title', 'author') # Add search capability for these fields
-
-admin.site.register(Book, BookAdmin)
+# --- UPDATED: Extend Django's default UserAdmin to work with CustomUser ---
+# Define a custom UserAdmin to display CustomUser fields
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    # These are the fields from AbstractUser that you want to display
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role')
+    
+    # These are the fields that will be editable on the user's admin page
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo', 'role')}),
+    )
