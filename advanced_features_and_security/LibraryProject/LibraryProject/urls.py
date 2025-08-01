5ec2bb54017e92d988bb790f43ef68bb64eccd73
Brian Miano <brianmiano1@gmail.com>
2025-08-02 00:37:59 +0100

    We added a custom user model changing it from the previous case where we were using Django's inbuilt user profile model

---
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -0,0 +1,21 @@
+# LibraryProject/LibraryProject/urls.py
+"""
+URL configuration for LibraryProject project.
+... (Django comments) ...
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect 
+
+# Helper function to redirect root to book list
+def redirect_to_book_list(request):
+    return redirect('relationship_app:book_list') # Redirects to the namespaced URL
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # This line correctly includes your app's URLs at the 'relationship/' path
+    path('relationship/', include('relationship_app.urls')),
+    # --- NEW: This handles the root URL ('/') ---
+    path('', redirect_to_book_list, name='home'),
+]
---
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -0,0 +1,25 @@
+# relationship_app/admin.py
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin 
+from .models import Author, Book, Library, Librarian, CustomUser # UPDATED: Import CustomUser
+from django.contrib.auth.models import Group # Import Group model to unregister it if needed
+
+# Register your existing models
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
+
+
+# --- UPDATED: Extend Django's default UserAdmin to work with CustomUser ---
+# Define a custom UserAdmin to display CustomUser fields
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    # These are the fields from AbstractUser that you want to display
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role')
+    
+    # These are the fields that will be editable on the user's admin page
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo', 'role')}),
+    )
