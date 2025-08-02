56ddc515908a4e44027503e0c187556353b37398
Brian Miano <brianmiano1@gmail.com>
2025-08-02 01:01:49 +0100

    the checker is checking for changes made to the bookshelf_app yet I had done changes to relationship_app

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf.apps.BookshelfConfig',
+    'bookshelf.CustomerUser',
     'relationship_app.apps.RelationshipAppConfig',
 ]
 
