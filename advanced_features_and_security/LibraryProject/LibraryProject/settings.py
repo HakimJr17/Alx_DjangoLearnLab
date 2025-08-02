583f29a67cf9f0d7a84e8f4f413ffbeeb910baa6
Brian Miano <brianmiano1@gmail.com>
2025-08-02 01:08:29 +0100

    the AUTH_USER_MODEL for bookshelf needed to be set to bookshelf.CustomUser

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf.apps.BookshelfConfig',
-    'bookshelf.CustomerUser',
     'relationship_app.apps.RelationshipAppConfig',
 ]
 
@@ -126,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
