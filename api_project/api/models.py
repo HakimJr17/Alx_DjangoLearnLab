0d9d60180ecddad68fc193c487f1049d1d22f12a
Brian Miano <brianmiano1@gmail.com>
2025-08-14 07:35:46 +0100

    I have creating a new API and just done the process of defining the models and making sure the server is working just perfect.

---
+++ api_project/api/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    '''
+    The purpose of this method is to ensure the Author class is represented in a well 
+    human-readable string representation of an object as opposed to the less descriptive
+    name <Author: Author object (1)>..
+    '''
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+
+    '''
+    The on_delete=models.CASCADE means that if an Author is deleted, 
+    all of their associated Book records will also be automatically deleted.
+    The instructions on ALX had initially stated that we just define tha author attribute inside 
+    the book class but the reasoning for differentiating them is that this approach enhances 
+    flexibility and scalability should if we wnated to add more attributes to our Author class
+    '''
